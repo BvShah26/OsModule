c027b34130546a3eb2af137710414228d8895d54
BvShah26 <92227795+BvShah26@users.noreply.github.com>
2021-10-09 23:56:06 +0530

    Add files via upload

---
+++ osTest.js
@@ -0,0 +1,7 @@
+var os = require('os');
+console.log("Os Info [OS Module]");
+console.log("OS CPU architecture :: " + os.arch());
+console.log("Free memory of the system :: " + Math.round(((os.freemem() / 1024 / 1024 / 1024) * 100)) / 100 + " - GB");
+console.log("Total memory of the system :: " + Math.round(((os.totalmem() / 1024 / 1024 / 1024) * 100)) / 100 + " - GB");
+console.log("OS Platform :: " + os.platform());
+console.log("Information about the current user :: " + os.userInfo().username);
